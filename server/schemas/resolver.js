a5190223efc6335d9fda2b60d7479210808c31a1
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-19 19:46:43 -0700

    add query and mutation

---
+++ server/schemas/resolver.js
@@ -0,0 +1,61 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return foundUser = await User.findOne({
+                    $or: [{ _id: context.user._id }, { username: context.user.username }],
+                }).populate('savedBooks');
+            }
+            throw new AuthenticationError("Couldn't find user with this id!");
+        }
+    },
+
+    Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email }).populate('savedBooks');
+            if (!user) {
+                throw new AuthenticationError("Can't find this user");
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Wrong password!');
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
+
+        saveBook: async (parent, {authors, description, bookId, image, link, title}, context) => {
+            if(context.user) {
+                return await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {savedBooks: {authors, description, bookId, image, link, title}}},
+                    { new: true, runValidators: true}
+                ).populate('savedBooks');
+            }
+            throw new AuthenticationError("Couldn't find user with this id!");
+        },
+
+        deleteBook: async (parent, { bookId }, context) => {
+            if(context.user) {
+                return await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId: bookId}}},
+                    { new: true }
+                ).populate('savedBooks');
+            }
+            throw new AuthenticationError("Couldn't find user with this id!");
+        }
+    }
+}
+
+module.exports = resolvers;
